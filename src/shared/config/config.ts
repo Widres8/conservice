ff0025b28c99de426872583aaa9b0ec0996ae5d5
widres8 <williandres8@gmail.com>
2024-09-06 20:59:01 -0500

    [main] update: whatsapp number

--- src/shared/config/config.ts
+++ src/shared/config/config.ts
@@ -1,6 +1,8 @@
+const whatsapp = `573212226600`;
 export const CONFIG = {
-    contact: '',
-    footer: 'Profesionales a tu servicio, ¡Su mejor elección en obras civiles!',
-    copyright: '© www.conservice.com.co Todos los derechos reservados',
-    copyrightDeveloper: 'Designed and Developed By [widres8](https://widres8.github.io/)',
+  contact: '',
+  footer: 'Profesionales a tu servicio, ¡Su mejor elección en obras civiles!',
+  copyright: '© www.conservice.com.co Todos los derechos reservados',
+  copyrightDeveloper: 'Designed and Developed By [widres8](https://widres8.github.io/)',
+  whatsappLink: `https://api.whatsapp.com/send?phone=${whatsapp}&text=Hola%20*CONSERVICE*.%20Necesito%20m%C3%A1s%20informaci%C3%B3n%20sobre%20CONSERVICE%20https%3A%2F%2Fconservice.com.co`,
 };
